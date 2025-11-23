051e137ca51f94af17ceccbad42ae1c292860bb8
andersonmelo18 <andersonevolution18@gmail.com>
2025-11-23 20:51:20 -0300

    Anderson

--- src/model/Cargo.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-enum CARGO {
-    Aluno,
-    Professor,
-    Coordenador,
-    Diretor
-}
--- src/model/interfaces/Folha.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-// Define um contrato que qualquer classe que a implementar deve seguir
-public interface Folha {
-    double calcularSalarioBruto();
-    double calcularImpostos();
-    double calcularSalarioLiquido();
-}
